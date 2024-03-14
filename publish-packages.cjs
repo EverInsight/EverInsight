760b90ad4e944a98b6e616bea9085dc25b843aab
Ben <ben@zfben.com>
2024-03-14 10:15:22 +0800

    fix cd

--- publish-packages.cjs
+++ publish-packages.cjs
@@ -4,15 +4,24 @@ const { readFileSync, writeFileSync } = require('node:fs')
 
 const newVersion = `${version}-devel-${Date.now()}`
 
+const appPackagePath = `${__dirname}/apps/app/package.json`
+const appPackage = JSON.parse(readFileSync(appPackagePath).toString())
+
 for (const name of [
   'mdx'
 ]) {
   const packagePath = `${__dirname}/packages/${name}/package.json`
+
   const pkg = JSON.parse(readFileSync(packagePath).toString())
   pkg.version = newVersion
+
   writeFileSync(packagePath, JSON.stringify(pkg, null, 2))
-  execSync(`npm publish -w packages/${name} --access public`, {
+
+  execSync(`npm publish -w packages/${name} --access public && npm dist-tag add ${pkg.name}@${newVersion} devel`, {
     stdio: 'inherit',
   })
-  execSync(`npm dist-tag add ${pkg.name}@${newVersion} devel`)
+
+  appPackage.dependencies[pkg.name] = newVersion
 }
+
+writeFileSync(appPackagePath, JSON.stringify(appPackage, null, 2))
